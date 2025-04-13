1ad12b32ed986d63625b3458e178282d2532de25
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-04-14 02:13:46 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,5 @@
-import jsonServer from "json-server";
-import path from "path";
+import data from '../data.json';
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), "data.json"));
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-
-export default server;
+export default function handler(req, res) {
+    res.status(200).json(data);
+}
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import { QuestionData } from "../types";
 
 export const fetchQuestions = async (): Promise<QuestionData> => {
   // const res = await fetch("http://localhost:3001/data");
-  const res = await fetch('/api/index');
+  const res = await fetch('/api');
   if (!res.ok) throw new Error("Failed to fetch questions");
   const data: QuestionData = await res.json();
   return data;
