5b0cdb9c4a653c6be7d08343d46ce6e02dd0117a
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-04-14 01:51:21 +0530

    first commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import { QuestionData } from "../types";
 
 export const fetchQuestions = async (): Promise<QuestionData> => {
-  const res = await fetch("http://localhost:3001/data");
+  // const res = await fetch("http://localhost:3001/data");
+  const res = await fetch('/api/questions');
   if (!res.ok) throw new Error("Failed to fetch questions");
   const data: QuestionData = await res.json();
   return data;
